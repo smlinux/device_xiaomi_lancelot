6f5694b916560ea3a0de9050417762419e087660
dansh <smartlinux@ya.ru>
2023-01-20 05:45:00 +0200

    commit orangefox 12.1 files

---
+++ twrp_lancelot.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := lancelot
+
+# Inherit from those products. Most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from device
+$(call inherit-product, device/xiaomi/lancelot/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := lancelot
+PRODUCT_NAME := twrp_lancelot
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := M2004J19C
+PRODUCT_MANUFACTURER := Xiaomi
